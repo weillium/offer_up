5f64912ae00ba3bbb8533e03e780809736c0bb9f
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-02-18 23:31:40 +0000

    Whitelist columns in Comment for ActiveAdmin

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Comment do
 
+ permit_params :item_id, :user_id, :comment
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
