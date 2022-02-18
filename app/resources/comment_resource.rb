fde2707c7db6c3a5055bc768979978d38bcfd07f
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-02-18 23:32:32 +0000

    Generate graphiti resources for comments

---
+++ app/resources/comment_resource.rb
@@ -0,0 +1,13 @@
+class CommentResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :item_id, :integer
+  attribute :user_id, :integer
+  attribute :comment, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
