42f8a833c25493d4db58142e2a3ee5c612862628
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-02-18 23:31:26 +0000

    Direct association between users and purchases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :purchases,
+             :dependent => :destroy
+
   has_many   :comments,
              :dependent => :destroy
 
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Purchase, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:user) }
+
     end
 
     describe "InDirect Associations" do
